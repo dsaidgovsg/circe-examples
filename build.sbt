f3b8b0c2c51a38eff0254c23f60af07e9d9dd963
Chen Weiguang <chen.weiguang@gmail.com>
2020-03-10 00:32:05 +0800

    Add circe examples with snake_case configuration.

--- build.sbt
+++ build.sbt
@@ -10,13 +10,18 @@ val circeVersion = "0.12.3"
 lazy val root = (project in file("."))
   .settings(
     name := "circe-examples",
+    scalacOptions ++= Seq(
+      "-Ymacro-annotations",
+    ),
     libraryDependencies ++=
-      Seq(scalaTest % Test) ++
       Seq(
         "io.circe" %% "circe-core",
         "io.circe" %% "circe-generic",
-        "io.circe" %% "circe-parser"
-      ).map(_ % circeVersion)
+        "io.circe" %% "circe-parser",
+      ).map(_ % circeVersion) ++
+      Seq(
+        "io.circe" %% "circe-generic-extras" % "0.12.2",
+      ),
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,9 +1,59 @@
 package example
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.generic.extras._
+import io.circe.parser._
+import io.circe.syntax._
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+// date_iterator
+
+object Hello extends App {
+  // This makes all members to be snake_case
+  implicit val config: Configuration = Configuration.default
+    .withSnakeCaseConstructorNames
+    .withSnakeCaseMemberNames
+
+  @ConfiguredJsonCodec
+  sealed trait Foo
+
+  @ConfiguredJsonCodec
+  case class BarVal(valInt: Int, valDbl: Option[Double]) extends Foo
+
+  @ConfiguredJsonCodec
+  case class BazVal(valVec: Vector[String]) extends Foo
+
+
+  //
+  // BarVal
+  //
+
+  val origBar: Foo = BarVal(13, Some(14.0))
+
+  val encodedBar = origBar.asJson.noSpaces
+  println(encodedBar)
+
+  val decodedBar = decode[Foo](encodedBar)
+  println(decodedBar)
+
+  val decodedBar2 = decode[Foo]("""{"bar_val":{"val_int":13,"val_dbl":null}}""")
+  println(decodedBar2)
+
+  val decodedBar3 = decode[Foo]("""{"bar_val":{"val_int":13}}""")
+  println(decodedBar3)
+
+  //
+  // BazVal
+  //
+
+  val origBaz: Foo = BazVal(Vector("123", "456"))
+
+  val encodedBaz = origBaz.asJson.noSpaces
+  println(encodedBaz)
+
+  val decodedBaz = decode[Foo](encodedBaz)
+  println(decodedBaz)
+
+  val decodedBaz2 = decode[Foo]("""{"baz_val":{"val_vec":[]}}""")
+  println(decodedBaz2)
 }
