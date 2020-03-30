4271a2c078c9819f42de186e3284f90bf3d5fcda
Chen Weiguang <chen.weiguang@gmail.com>
2020-03-30 10:16:19 +0800

    Add Func helper arity to 10.

--- extras/Func.scala
+++ extras/Func.scala
@@ -36,4 +36,67 @@ object Func {
 
   def mapResult[I1, I2, I3, O1, O2](f: (I1, I2, I3) => O1)(g: O1 => O2): (I1, I2, I3) => O2 =
     (i1: I1, i2: I2, i3: I3) => g(f(i1, i2, i3))
+
+  // Arity-4
+  
+  implicit class Arity4[I1, I2, I3, I4, O1, O2](f: (I1, I2, I3, I4) => O1) {
+    def mapResult(g: O1 => O2) = Func.mapResult(f)(g)
+  }
+
+  def mapResult[I1, I2, I3, I4, O1, O2](f: (I1, I2, I3, I4) => O1)(g: O1 => O2): (I1, I2, I3, I4) => O2 =
+    (i1: I1, i2: I2, i3: I3, i4: I4) => g(f(i1, i2, i3, i4))
+
+  // Arity-5
+  
+  implicit class Arity5[I1, I2, I3, I4, I5, O1, O2](f: (I1, I2, I3, I4, I5) => O1) {
+    def mapResult(g: O1 => O2) = Func.mapResult(f)(g)
+  }
+
+  def mapResult[I1, I2, I3, I4, I5, O1, O2](f: (I1, I2, I3, I4, I5) => O1)(g: O1 => O2): (I1, I2, I3, I4, I5) => O2 =
+    (i1: I1, i2: I2, i3: I3, i4: I4, i5: I5) => g(f(i1, i2, i3, i4, i5))
+
+  // Arity-6
+  
+  implicit class Arity6[I1, I2, I3, I4, I5, I6, O1, O2](f: (I1, I2, I3, I4, I5, I6) => O1) {
+    def mapResult(g: O1 => O2) = Func.mapResult(f)(g)
+  }
+
+  def mapResult[I1, I2, I3, I4, I5, I6, O1, O2](f: (I1, I2, I3, I4, I5, I6) => O1)(g: O1 => O2): (I1, I2, I3, I4, I5, I6) => O2 =
+    (i1: I1, i2: I2, i3: I3, i4: I4, i5: I5, i6: I6) => g(f(i1, i2, i3, i4, i5, i6))
+
+  // Arity-7
+  
+  implicit class Arity7[I1, I2, I3, I4, I5, I6, I7, O1, O2](f: (I1, I2, I3, I4, I5, I6, I7) => O1) {
+    def mapResult(g: O1 => O2) = Func.mapResult(f)(g)
+  }
+
+  def mapResult[I1, I2, I3, I4, I5, I6, I7, O1, O2](f: (I1, I2, I3, I4, I5, I6, I7) => O1)(g: O1 => O2): (I1, I2, I3, I4, I5, I6, I7) => O2 =
+    (i1: I1, i2: I2, i3: I3, i4: I4, i5: I5, i6: I6, i7: I7) => g(f(i1, i2, i3, i4, i5, i6, i7))
+
+  // Arity-8
+  
+  implicit class Arity8[I1, I2, I3, I4, I5, I6, I7, I8, O1, O2](f: (I1, I2, I3, I4, I5, I6, I7, I8) => O1) {
+    def mapResult(g: O1 => O2) = Func.mapResult(f)(g)
+  }
+
+  def mapResult[I1, I2, I3, I4, I5, I6, I7, I8, O1, O2](f: (I1, I2, I3, I4, I5, I6, I7, I8) => O1)(g: O1 => O2): (I1, I2, I3, I4, I5, I6, I7, I8) => O2 =
+    (i1: I1, i2: I2, i3: I3, i4: I4, i5: I5, i6: I6, i7: I7, i8: I8) => g(f(i1, i2, i3, i4, i5, i6, i7, i8))
+
+  // Arity-9
+  
+  implicit class Arity9[I1, I2, I3, I4, I5, I6, I7, I8, I9, O1, O2](f: (I1, I2, I3, I4, I5, I6, I7, I8, I9) => O1) {
+    def mapResult(g: O1 => O2) = Func.mapResult(f)(g)
+  }
+
+  def mapResult[I1, I2, I3, I4, I5, I6, I7, I8, I9, O1, O2](f: (I1, I2, I3, I4, I5, I6, I7, I8, I9) => O1)(g: O1 => O2): (I1, I2, I3, I4, I5, I6, I7, I8, I9) => O2 =
+    (i1: I1, i2: I2, i3: I3, i4: I4, i5: I5, i6: I6, i7: I7, i8: I8, i9: I9) => g(f(i1, i2, i3, i4, i5, i6, i7, i8, i9))
+
+  // Arity-10
+  
+  implicit class Arity10[I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, O1, O2](f: (I1, I2, I3, I4, I5, I6, I7, I8, I9, I10) => O1) {
+    def mapResult(g: O1 => O2) = Func.mapResult(f)(g)
+  }
+
+  def mapResult[I1, I2, I3, I4, I5, I6, I7, I8, I9, I10, O1, O2](f: (I1, I2, I3, I4, I5, I6, I7, I8, I9, I10) => O1)(g: O1 => O2): (I1, I2, I3, I4, I5, I6, I7, I8, I9, I10) => O2 =
+    (i1: I1, i2: I2, i3: I3, i4: I4, i5: I5, i6: I6, i7: I7, i8: I8, i9: I9, i10: I10) => g(f(i1, i2, i3, i4, i5, i6, i7, i8, i9, i10))
 }
