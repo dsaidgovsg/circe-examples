bb1eda066f2f6ef96b28f583db0f6b456fd780c6
Chen Weiguang <chen.weiguang@gmail.com>
2020-04-07 14:26:10 +0800

    feat(adt): use more sealed traits to union
    Showcase how to group a branch of enum variants using additional
    deriving sealed traits.

--- util/Base.scala
+++ util/Base.scala
@@ -8,10 +8,21 @@ import io.circe.syntax.EncoderOps  // .asJson needs this
 import circeeg.extras.{CirceEnumVariant, delegate}
 import circeeg.util.Conf.custom
 
+// Scala has a fairly cheat way to "union" the ADT variants if the variants
+// happen to have additional tiering.
+// Just use additional sealed traits inheriting base sealed traits for this
+
 @ConfiguredJsonCodec
 sealed trait Base extends Useless
 
-object Base {
+@ConfiguredJsonCodec
+sealed trait B1 extends Base
+
+@ConfiguredJsonCodec
+sealed trait B2 extends Base
+
+
+object B1 {
   // The case class (enum component) names below do not actually have to
   // be exactly the same as the ones in Xyz.scala.
   // They are named the same to easily correlate the two
@@ -21,29 +32,33 @@ object Base {
   // but at least we have a second-say on how the enum component here is named
 
   @CirceEnumVariant
-  final case class X(@delegate v: circeeg.util.X) extends Base
+  final case class X(@delegate v: circeeg.util.X) extends B1
 
   @CirceEnumVariant
-  final case class Y(@delegate v: circeeg.util.Y) extends Base
+  final case class Y(@delegate v: circeeg.util.Y) extends B1
+}
 
+object B2 {
   @CirceEnumVariant
-  final case class Z(@delegate v: circeeg.util.Z) extends Base
+  final case class Z(@delegate v: circeeg.util.Z) extends B2
 
   // A uses arity-3
   @CirceEnumVariant
-  final case class A(@delegate v: circeeg.util.A) extends Base
+  final case class A(@delegate v: circeeg.util.A) extends B2
 
   @CirceEnumVariant(case_class_fwd = false)
-  final case class B(v: String) extends Base {
+  final case class B(v: String) extends B2 {
     def foo(): String = "abc"
     def foo(x: Int): Int = id + 1
     def id: Int = 777
   }
 
   @CirceEnumVariant(case_class_fwd = false)
-  final case class C(v: NonEmptyList[Int]) extends Base {
+  final case class C(v: NonEmptyList[Int]) extends B2 {
     def foo(): String = "NonEmptyList"
     def foo(x: Int): Int = id + 1
     def id: Int = 88
   }
 }
+
+object Base
