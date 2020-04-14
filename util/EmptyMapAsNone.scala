54ffcdb9e9a06d79305310403bcdb910004043b7
Weiguang <chen.weiguang@gmail.com>
2020-04-14 12:10:49 +0800

    feat(eman): add empty-map-as-none type (#10)
    This type accept empty JSON object as None, and decodes back the same
    way if it is None.

---
+++ util/EmptyMapAsNone.scala
@@ -0,0 +1,33 @@
+package circeeg.util
+
+import io.circe.{Decoder, Encoder, HCursor, Json}
+import scala.util.{Left, Right}
+
+case class EmptyMapAsNone[A](val toOption: Option[A])
+
+object EmptyMapAsNone {
+  import cats.syntax.either._
+  import io.circe.syntax._
+
+  final def apply[A](x: A): EmptyMapAsNone[A] = EmptyMapAsNone[A](Option(x))
+  final def empty[A]: EmptyMapAsNone[A] = EmptyMapAsNone[A](None)
+
+  implicit def emanEncoder[A: Encoder]: Encoder[EmptyMapAsNone[A]] = new Encoder[EmptyMapAsNone[A]] {
+    final def apply(x: EmptyMapAsNone[A]): Json =
+      if (x.toOption.isEmpty) {
+          // It can be any key type since the value is meant to be empty
+          Map[String, A]().asJson
+      } else {
+          x.toOption.asJson
+      }
+  }
+
+  implicit def emanDecoder[A: Decoder]: Decoder[EmptyMapAsNone[A]] = new Decoder[EmptyMapAsNone[A]] {
+    final def apply(x: HCursor): Decoder.Result[EmptyMapAsNone[A]] =
+      // It can be any key type since the value is meant to be empty
+      x.as[Map[String, A]] match {
+        case Right(v) if v.isEmpty => Right(EmptyMapAsNone.empty)  // Try empty dict first
+        case _ => x.as[Option[A]].right.map(EmptyMapAsNone(_)) // Try Option[A] last (we accept null as None too)
+      }
+  }
+}
